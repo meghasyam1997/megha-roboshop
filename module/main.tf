0c5dd87907bd16fd0a6bffccb0e0e4c0ba4f78b7
meghasyam1997 <meghasyam01@gmail.com>
2024-01-09 09:43:11 +0530

    frontend

--- module/main.tf
+++ module/main.tf
@@ -2,13 +2,14 @@ resource "aws_instance" "instance" {
   instance_type          = var.instance_type
   ami                    = data.aws_ami.ami.id
   vpc_security_group_ids = [data.aws_security_group.allow_all.id]
-  iam_instance_profile   = aws_iam_instance_profile.iam_profile.name
+#  iam_instance_profile   = aws_iam_instance_profile.iam_profile.name
 
   tags = {
     Name = local.name
   }
 }
 resource "null_resource" "provisioner" {
+  count = var.provisioner ? 1 : 0
   depends_on = [aws_instance.instance, aws_route53_record.records]
   provisioner "remote-exec" {
     connection {
@@ -17,7 +18,13 @@ resource "null_resource" "provisioner" {
       password = "DevOps321"
       host     = aws_instance.instance.private_ip
     }
-    inline = var.app_type == "db" ? local.db_command : local.app_command
+#    inline = var.app_type == "db" ? local.db_command : local.app_command
+    inline = [
+      "rm -rf roboshop-shell",
+      "git clone https://github.com/meghasyam1997/roboshop-shell.git ",
+      "cd roboshop-shell",
+      "sudo bash ${var.component_name}.sh ${var.password}"
+    ]
   }
 }
 
@@ -29,57 +36,57 @@ resource "aws_route53_record" "records" {
   records = [aws_instance.instance.private_ip]
 }
 
-resource "aws_iam_role" "role" {
-  name = "${var.env}-${var.component_name}-role"
-
-  # Terraform's "jsonencode" function converts a
-  # Terraform expression result to valid JSON syntax.
-  assume_role_policy = jsonencode({
-    Version   = "2012-10-17"
-    Statement = [
-      {
-        Action    = "sts:AssumeRole"
-        Effect    = "Allow"
-        Sid       = ""
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-      },
-    ]
-  })
-
-  tags = {
-    tag-key = "${var.component_name}-${var.env}"
-  }
-}
-
-resource "aws_iam_instance_profile" "iam_profile" {
-  name = "${var.component_name}-${var.env}"
-  role = aws_iam_role.role.name
-}
-
-resource "aws_iam_role_policy" "ssm-ps-policy" {
-  name = "${var.component_name}-${var.env}-ssm-ps-policy"
-  role = aws_iam_role.role.id
-
-  policy = jsonencode({
-    "Version" : "2012-10-17",
-    "Statement" : [
-      {
-        "Sid" : "VisualEditor0",
-        "Effect" : "Allow",
-        "Action" : [
-          "kms:Decrypt",
-          "ssm:GetParameterHistory",
-          "ssm:GetParametersByPath",
-          "ssm:GetParameters",
-          "ssm:GetParameter"
-        ],
-        "Resource" : [
-          "arn:aws:ssm:us-east-1:561174155654:parameter/${var.env}.${var.component_name}.*",
-          "arn:aws:kms:us-east-1:561174155654:key/54baa543-c347-432e-ab40-108d5d0b67bb"
-        ]
-      }
-    ]
-  })
-}
+#resource "aws_iam_role" "role" {
+#  name = "${var.env}-${var.component_name}-role"
+#
+#  # Terraform's "jsonencode" function converts a
+#  # Terraform expression result to valid JSON syntax.
+#  assume_role_policy = jsonencode({
+#    Version   = "2012-10-17"
+#    Statement = [
+#      {
+#        Action    = "sts:AssumeRole"
+#        Effect    = "Allow"
+#        Sid       = ""
+#        Principal = {
+#          Service = "ec2.amazonaws.com"
+#        }
+#      },
+#    ]
+#  })
+#
+#  tags = {
+#    tag-key = "${var.component_name}-${var.env}"
+#  }
+#}
+#
+#resource "aws_iam_instance_profile" "iam_profile" {
+#  name = "${var.component_name}-${var.env}"
+#  role = aws_iam_role.role.name
+#}
+#
+#resource "aws_iam_role_policy" "ssm-ps-policy" {
+#  name = "${var.component_name}-${var.env}-ssm-ps-policy"
+#  role = aws_iam_role.role.id
+#
+#  policy = jsonencode({
+#    "Version" : "2012-10-17",
+#    "Statement" : [
+#      {
+#        "Sid" : "VisualEditor0",
+#        "Effect" : "Allow",
+#        "Action" : [
+#          "kms:Decrypt",
+#          "ssm:GetParameterHistory",
+#          "ssm:GetParametersByPath",
+#          "ssm:GetParameters",
+#          "ssm:GetParameter"
+#        ],
+#        "Resource" : [
+#          "arn:aws:ssm:us-east-1:561174155654:parameter/${var.env}.${var.component_name}.*",
+#          "arn:aws:kms:us-east-1:561174155654:key/54baa543-c347-432e-ab40-108d5d0b67bb"
+#        ]
+#      }
+#    ]
+#  })
+#}
--- module/vars.tf
+++ module/vars.tf
@@ -2,4 +2,7 @@ variable "env" {}
 variable "component_name" {}
 variable "instance_type" {}
 variable "password" {}
-variable "app_type" {}
+variable "provisioner" {
+  default = false
+}
+#variable "app_type" {}
